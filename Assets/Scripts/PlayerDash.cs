9c268a6f5829fef5d608594649b2a1d6f9458cea
Rhys <1022076@student.sae.edu.au>
2022-03-30 17:23:47 +0800

    Movement & Camera
    Implemented basic movement and a rough dash, as well as camera follow

---
+++ Assets/Scripts/PlayerDash.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+    public float dashSpeed;
+    Rigidbody rb;
+    private bool isDashing = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !isDashing)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        isDashing = true;
+        rb.AddForce(transform.forward * dashSpeed, ForceMode.Impulse);
+        yield return new WaitForSeconds(0.5f);
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        //yield return new WaitForSeconds(2f);
+        isDashing = false;
+    }
+}
