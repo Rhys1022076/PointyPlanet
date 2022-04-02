7143d5991a96e549ff4b10556e3f7413cb94a8ec
Rhys <rhysvc@iinet.net.au>
2022-04-02 18:46:52 +0800

    Tests and Tweaks
    Implemented new Spike Model, created idle and walk animations for Prickles, temporarily imported a particle fx pack - using two for the dash effect. Updated Player prefab

--- Assets/Scripts/PlayerDash.cs
+++ Assets/Scripts/PlayerDash.cs
@@ -7,6 +7,8 @@ public class PlayerDash : MonoBehaviour
     public float dashSpeed;
     Rigidbody rb;
     private bool isDashing = false;
+    public ParticleSystem dashTrail;
+    public ParticleSystem dashFx;
 
     private void Start()
     {
@@ -25,10 +27,22 @@ public class PlayerDash : MonoBehaviour
     {
         isDashing = true;
         rb.AddForce(transform.forward * dashSpeed, ForceMode.Impulse);
+        StartDashFX();
         yield return new WaitForSeconds(0.3f);
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
+        StopDashFX();
         yield return new WaitForSeconds(2f);
         isDashing = false;
+    }
+    void StartDashFX()
+    {
+        dashTrail.Play();
+        dashFx.Play();
+    }
+    void StopDashFX()
+    {
+        dashTrail.Stop();
+        dashFx.Stop();
     }
 }
