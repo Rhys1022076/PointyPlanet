9413e9edd04a1fff2cbf243167adc11ab3da047e
Rhys <rhysvc@iinet.net.au>
2022-04-23 23:47:47 +0800

    FMOD Sounds
    Added in audio files from Google Drive

---
+++ Assets/Scripts/TriggerAudio.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudio : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+	public string Event;
+
+	public bool PlayOnAwake;
+	public bool PlayOnDestroy;
+
+	private void Start()
+	{
+		if (PlayOnAwake)
+		{
+			PlayOneShot();
+		}
+	}
+
+	public void PlaySound(string path)
+	{
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+	}
+
+	public void PlayOneShot()
+	{
+		FMODUnity.RuntimeManager.PlayOneShotAttached(Event, gameObject);
+	}
+
+	private void OnDestroy()
+	{
+		if (PlayOnDestroy)
+		{
+			PlayOneShot();
+		}
+	}
+}
